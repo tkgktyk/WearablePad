05fcf159e0aee07533bcfe1e6f329694936c39e8
tkgktyk <tkgktyk@gmail.com>
2015-05-12 14:58:41 +0900

    support screen rotation

--- mobile/src/main/java/jp/tkgktyk/wearablepad/MyService.java
+++ mobile/src/main/java/jp/tkgktyk/wearablepad/MyService.java
@@ -18,11 +18,14 @@ package jp.tkgktyk.wearablepad;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.view.KeyEvent;
+import android.view.Surface;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -45,6 +48,11 @@ import jp.tkgktyk.wearablepadlib.TouchMessage;
 
 /**
  * Created by tkgktyk on 2015/04/27.
+ * <p/>
+ * Basically use float point for scale (relative) value based on degree=0 (portraite),
+ * and double point (x,y) for absolute value based on degree=0,
+ * and int or short point (x,y) for rotated value.
+ * Not need to transform for Cursor and Message position.
  */
 public class MyService extends WearableListenerService {
     /**
@@ -62,25 +70,77 @@ public class MyService extends WearableListenerService {
 
     private static final int MAX_DISTANCE = 100;
 
-    private static final String KEY_LAST_CURSOR_X = "last_cursor_x";
-    private static final String KEY_LAST_CURSOR_Y = "last_cursor_y";
+    public static final String KEY_LAST_CURSOR_X = "key_last_cursor_x";
+    public static final String KEY_LAST_CURSOR_Y = "key_last_cursor_y";
+    private static final float DEFAULT_CURSOR_POSITION = 0.5f;
 
     private PowerManager.WakeLock mWakeLock;
     private Settings mSettings;
 
-    private int mCursorX;
-    private int mCursorY;
-    private int mSwipeCursorX;
-    private int mSwipeCursorY;
+    private PointF mCursor = new PointF();
+    private PointF mSwipeCursor = new PointF();
 
-    private Point mDisplaySize;
+    private Point mDisplaySize = new Point();
     private float mMaxDistance;
     private WindowManager mWindowManager;
     private ImageView mCursorView;
-    private Point mCursorSize;
+    private Point mCursorSize = new Point();
     private WindowManager.LayoutParams mLayoutParams;
 
     private FileOutputStream mInputDevice;
+
+    private int mScreenRotation;
+
+    private Point getRotatedPointForInputDevice(PointF point) {
+        float x = 0.0f;
+        float y = 0.0f;
+        switch (mScreenRotation) {
+            case Surface.ROTATION_0:
+                x = point.x;
+                y = point.y;
+                break;
+            case Surface.ROTATION_90:
+                x = 1.0f - point.y;
+                y = point.x;
+                break;
+            case Surface.ROTATION_180:
+                x = 1.0f - point.x;
+                y = 1.0f - point.y;
+                break;
+            case Surface.ROTATION_270:
+                x = point.y;
+                y = 1.0f - point.x;
+                break;
+            default:
+                MyApp.logD();
+        }
+        final float sizeX = Math.min(mDisplaySize.x, mDisplaySize.y);
+        final float sizeY = Math.max(mDisplaySize.x, mDisplaySize.y);
+        return new Point(Math.round(x * sizeX * mSettings.ratioX),
+                Math.round(y * sizeY * mSettings.ratioY));
+    }
+
+    private Point getRotatedPointForCursor(PointF point) {
+        return new Point(Math.round(point.x * mDisplaySize.x),
+                Math.round(point.y * mDisplaySize.y));
+    }
+
+    private void performPress(PointF point, ArrayList<byte[]> cmds) {
+        final Point rotated = getRotatedPointForInputDevice(point);
+        MyApp.logD(rotated.toString());
+        cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_X, rotated.x));
+        cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_Y, rotated.y));
+        cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
+    }
+
+    private void performTap(ArrayList<byte[]> cmds) {
+        // down
+        cmds.add(makeEvent(EV_ABS, ABS_MT_TRACKING_ID, (int) System.currentTimeMillis()));
+        performPress(mCursor, cmds);
+        // up
+        cmds.add(makeEvent(EV_ABS, ABS_MT_TRACKING_ID, 0xFFFFFFFF));
+        cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
+    }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
@@ -96,58 +156,42 @@ public class MyService extends WearableListenerService {
         ArrayList<byte[]> cmds = Lists.newArrayList();
         switch (message.getMaskedEvent()) {
             case TouchMessage.EVENT_SHOW_CURSOR:
-                mCursorView.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mCursorView.setAlpha(1.0f);
-                        mLayoutParams.alpha = 1.0f;
-                        mCursorView.invalidate();
-                        updateCursorView();
-                    }
-                });
+//                mCursorView.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        mCursorView.setAlpha(1.0f);
+//                        mLayoutParams.alpha = 1.0f;
+//                        mCursorView.invalidate();
+//                        updateCursorView();
+//                    }
+//                });
                 break;
             case TouchMessage.EVENT_START_DRAG:
                 cmds.add(makeEvent(EV_ABS, ABS_MT_TRACKING_ID, (int) System.currentTimeMillis()));
-                mSwipeCursorX = mCursorX;
-                mSwipeCursorY = mCursorY;
-                cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_X,
-                        Math.round(mSwipeCursorX * mSettings.ratioX)
-                ));
-                cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_Y,
-                        Math.round(mSwipeCursorY * mSettings.ratioY)
-                ));
-                cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
+                mSwipeCursor.x = mCursor.x;
+                mSwipeCursor.y = mCursor.y;
+                performPress(mSwipeCursor, cmds);
                 // drop
             case TouchMessage.EVENT_DRAG:
-                final int newX = clampX(mSwipeCursorX + message.x * mSettings.speed);
-                final int newY = clampY(mSwipeCursorY + message.y * mSettings.speed);
-                final int dx = newX - mSwipeCursorX;
-                final int dy = newY - mSwipeCursorY;
-                final int distance = (int) Math.round(Math.sqrt(dx * dx + dy * dy));
+                final float newX = clamp(mSwipeCursor.x + message.x * mSettings.speed / mDisplaySize.x);
+                final float newY = clamp(mSwipeCursor.y + message.y * mSettings.speed / mDisplaySize.y);
+                final double dx = (newX - mSwipeCursor.x) * mDisplaySize.x; // absolute
+                final double dy = (newY - mSwipeCursor.y) * mDisplaySize.y; // absolute
+                final double distance = Math.sqrt(dx * dx + dy * dy);
                 final int n = (int) (distance / mMaxDistance) + 1; // ceil
                 for (int i = 0; i < n; ++i) {
-                    cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_X,
-                            Math.round((mSwipeCursorX + dx * (i + 1) / n) * mSettings.ratioX)
-                    ));
-                    cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_Y,
-                            Math.round((mSwipeCursorY + dy * (i + 1) / n) * mSettings.ratioY)
-                    ));
-                    cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
+                    mSwipeCursor.x += dx / n / mDisplaySize.x;
+                    mSwipeCursor.y += dy / n / mDisplaySize.y;
+                    performPress(mSwipeCursor, cmds);
                 }
-                mSwipeCursorX = newX;
-                mSwipeCursorY = newY;
-                cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_X,
-                        Math.round(mSwipeCursorX * mSettings.ratioX)
-                ));
-                cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_Y,
-                        Math.round(mSwipeCursorY * mSettings.ratioY)
-                ));
-                cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
+                mSwipeCursor.x = newX;
+                mSwipeCursor.y = newY;
+                performPress(mSwipeCursor, cmds);
 //                updateCursorView();
                 break;
             case TouchMessage.EVENT_MOVE:
-                mCursorX = clampX(mCursorX + message.x * mSettings.speed);
-                mCursorY = clampY(mCursorY + message.y * mSettings.speed);
+                mCursor.x = clamp(mCursor.x + message.x * mSettings.speed / mDisplaySize.x);
+                mCursor.y = clamp(mCursor.y + message.y * mSettings.speed / mDisplaySize.y);
                 updateCursorView();
                 break;
             case TouchMessage.EVENT_END_STROKE:
@@ -156,13 +200,7 @@ public class MyService extends WearableListenerService {
                 break;
             case TouchMessage.EVENT_PRESS:
                 cmds.add(makeEvent(EV_ABS, ABS_MT_TRACKING_ID, (int) System.currentTimeMillis()));
-                cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_X,
-                        Math.round(mCursorX * mSettings.ratioX)
-                ));
-                cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_Y,
-                        Math.round(mCursorY * mSettings.ratioY)
-                ));
-                cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
+                performPress(mCursor, cmds);
 //                updateCursorView();
                 break;
             case TouchMessage.EVENT_ACTION_TAP:
@@ -203,33 +241,32 @@ public class MyService extends WearableListenerService {
                 }
                 break;
             case TouchMessage.EVENT_ACTION_SWIPE:
-                final int x1 = mCursorX;
-                final int y1 = mCursorY;
+                final Point point1 = getRotatedPointForCursor(mCursor);
                 final int div = 3;
-                int x2 = x1;
-                int y2 = y1;
+                final Point point2 = new Point(point1);
                 switch (message.getActionValue()) {
                     case TouchMessage.SWIPE_LEFT_TO_RIGHT:
-                        x2 = clampX(x1 + mDisplaySize.x / div);
+                        point2.x = clamp(point1.x + mDisplaySize.x / div, mDisplaySize.x);
                         break;
                     case TouchMessage.SWIPE_TOP_TO_BOTTOM:
-                        y2 = clampY(y1 + mDisplaySize.y / div);
+                        point2.y = clamp(point1.y + mDisplaySize.y / div, mDisplaySize.y);
                         break;
                     case TouchMessage.SWIPE_RIGHT_TO_LEFT:
-                        x2 = clampX(x1 - mDisplaySize.x / div);
+                        point2.x = clamp(point1.x - mDisplaySize.x / div, mDisplaySize.x);
                         break;
                     case TouchMessage.SWIPE_BOTTOM_TO_TOP:
-                        y2 = clampY(y1 - mDisplaySize.y / div);
+                        point2.y = clamp(point1.y - mDisplaySize.y / div, mDisplaySize.y);
                         break;
                 }
-                Shell.SU.run(String.format("input swipe %d %d %d %d", x1, y1, x2, y2));
+                Shell.SU.run(String.format("input swipe %d %d %d %d",
+                        point1.x, point1.y, point2.x, point2.y));
                 break;
         }
         try {
             for (byte[] cmd : cmds) {
                 MyApp.logD("cmd.length = " + cmd.length);
                 StringBuilder sb = new StringBuilder(cmd.length * 2);
-                for(byte b: cmd) {
+                for (byte b : cmd) {
                     sb.append(String.format("%02x", b & 0xff));
                 }
                 MyApp.logD(sb.toString());
@@ -240,39 +277,24 @@ public class MyService extends WearableListenerService {
         }
     }
 
-    private void performTap(ArrayList<byte[]> cmds) {
-        // down
-        cmds.add(makeEvent(EV_ABS, ABS_MT_TRACKING_ID, (int) System.currentTimeMillis()));
-        cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_X,
-                Math.round(mCursorX * mSettings.ratioX)
-        ));
-        cmds.add(makeEvent(EV_ABS, ABS_MT_POSITION_Y,
-                Math.round(mCursorY * mSettings.ratioY)
-        ));
-        cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
-        // up
-        cmds.add(makeEvent(EV_ABS, ABS_MT_TRACKING_ID, 0xFFFFFFFF));
-        cmds.add(makeEvent(EV_SYN, SYN_REPORT, 0));
+    private float clamp(float scale) {
+        if (scale < 0.0f) {
+            return 0.0f;
+        }
+        if (scale > 1.0f) {
+            return 1.0f;
+        }
+        return scale;
     }
 
-    private int clampX(float newPosition) {
-        if (newPosition < 0) {
+    private int clamp(int rotatedValue, int max) {
+        if (rotatedValue < 0) {
             return 0;
         }
-        if (newPosition >= mDisplaySize.x) {
-            return mDisplaySize.x - 1;
+        if (rotatedValue > max) {
+            return max;
         }
-        return Math.round(newPosition);
-    }
-
-    private int clampY(float newPosition) {
-        if (newPosition < 0) {
-            return 0;
-        }
-        if (newPosition >= mDisplaySize.y) {
-            return mDisplaySize.y - 1;
-        }
-        return Math.round(newPosition);
+        return rotatedValue;
     }
 
     private byte[] makeEvent(short type, short code, int value) {
@@ -282,7 +304,7 @@ public class MyService extends WearableListenerService {
                 .allocate(headerSize + (Short.SIZE * 2 + Integer.SIZE) / Byte.SIZE)
                 .order(ByteOrder.LITTLE_ENDIAN);
         for (int i = 0; i < headerSize; ++i) {
-            buf.put((byte)0);
+            buf.put((byte) 0);
         }
         buf.putShort(type);
         buf.putShort(code);
@@ -301,8 +323,9 @@ public class MyService extends WearableListenerService {
     }
 
     private void setLayoutParams() {
-        mLayoutParams.x = Math.round((mCursorX - mDisplaySize.x / 2)) + mCursorSize.x;
-        mLayoutParams.y = Math.round((mCursorY - mDisplaySize.y / 2)) + mCursorSize.y;
+        final Point rotated = getRotatedPointForCursor(mCursor);
+        mLayoutParams.x = rotated.x - mDisplaySize.x / 2 + mCursorSize.x;
+        mLayoutParams.y = rotated.y - mDisplaySize.y / 2 + mCursorSize.y;
     }
 
     @Override
@@ -319,9 +342,10 @@ public class MyService extends WearableListenerService {
         mSettings = new Settings(this);
 
         mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        mDisplaySize = new Point();
-        mWindowManager.getDefaultDisplay().getRealSize(mDisplaySize);
         mMaxDistance = getResources().getDisplayMetrics().density * MAX_DISTANCE;
+
+        // after getting window manager
+        updateScreenRotation();
 
         // call after loading display size
         initCursorView();
@@ -333,13 +357,25 @@ public class MyService extends WearableListenerService {
         }
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        MyApp.logD();
+
+        updateScreenRotation();
+    }
+
+    private void updateScreenRotation() {
+        mWindowManager.getDefaultDisplay().getRealSize(mDisplaySize);
+        mScreenRotation = mWindowManager.getDefaultDisplay().getRotation();
+    }
+
     private void initCursorView() {
         // restore cursor position
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        mCursorX = prefs.getInt(KEY_LAST_CURSOR_X, mDisplaySize.x / 2);
-        mCursorY = prefs.getInt(KEY_LAST_CURSOR_Y, mDisplaySize.y / 2);
+        mCursor.x = prefs.getFloat(KEY_LAST_CURSOR_X, DEFAULT_CURSOR_POSITION);
+        mCursor.y = prefs.getFloat(KEY_LAST_CURSOR_Y, DEFAULT_CURSOR_POSITION);
         // make cursor
-        mCursorSize = new Point();
         mLayoutParams = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
@@ -359,8 +395,8 @@ public class MyService extends WearableListenerService {
                 updateCursorView();
             }
         });
-        mCursorView.setAlpha(0.0f);
-        mLayoutParams.alpha = 0.0f;
+//        mCursorView.setAlpha(0.0f);
+//        mLayoutParams.alpha = 0.0f;
         mWindowManager.addView(mCursorView, mLayoutParams);
     }
 
@@ -371,8 +407,8 @@ public class MyService extends WearableListenerService {
             mLayoutParams = null;
             // store cursor position
             PreferenceManager.getDefaultSharedPreferences(this).edit()
-                    .putInt(KEY_LAST_CURSOR_X, mCursorX)
-                    .putInt(KEY_LAST_CURSOR_Y, mCursorY)
+                    .putFloat(KEY_LAST_CURSOR_X, mCursor.x)
+                    .putFloat(KEY_LAST_CURSOR_Y, mCursor.y)
                     .apply();
         }
     }
